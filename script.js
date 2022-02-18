b5dc2b6b9bd77516d258a0c3f0d6565f32805ea8
zakiul fikri <59876777+zakiyul@users.noreply.github.com>
2022-02-18 16:04:28 +0800

    mobile first styling and sum javascript

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const humburgerIcon = document.querySelector('.bi-list');
 const closeIcon = document.querySelector('.bi-x-lg');
+const dropdownBtns = document.querySelectorAll('.list-dropdown');
 
 closeIcon.style.display = 'none';
 
@@ -12,3 +13,16 @@ closeIcon.addEventListener('click', function(event){
     humburgerIcon.style.display = 'block';
     closeIcon.style.display = 'none';
 })
+
+for(btn of dropdownBtns){
+    btn.addEventListener('click', function(event){
+        const listYgDiDapat = event.target;
+        if(listYgDiDapat.classList.contains("Active")){
+            listYgDiDapat.classList.remove("Active");
+            listYgDiDapat.classList.add("list-dropdown")
+        }else{
+            listYgDiDapat.classList.add("Active");
+        }
+        console.log(listYgDiDapat)
+    })
+}
